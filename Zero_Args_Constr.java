652d152d8049d87c9f67d735b6a55d5176deab87
Ashi Gupta <ashig9981@gmail.com>
2024-10-03 15:15:36 +0530

    Add files via upload
    question 2.

---
+++ Zero_Args_Constr.java
@@ -0,0 +1,15 @@
+class Zero_Args_Constr{
+    
+        // Our Zero-arguments constructor
+        Zero_Args_Constr() {
+            System.out.println("Zero-arguments constructor called.");
+        }
+    
+        public static void main(String[] args) {
+            // Creating an object 
+            Zero_Args_Constr obj = new  Zero_Args_Constr();
+           
+            
+        }
+    }
+    
