44e9e71ad200f0dda6f5baf680b96f9c015e7308
Ashi Gupta <ashig9981@gmail.com>
2024-11-19 13:27:22 +0530

    Add files via upload

---
+++ Para_Constr.java
@@ -0,0 +1,10 @@
+public class Para_Constr {
+          public Para_Constr(int num) {
+            System.out.println("The number is: " + num);
+        }    
+        public static void main(String[] args) {
+            Para_Constr d = new Para_Constr(42); 
+        }
+    }
+    
+
