b79c3489ed01f7bd15f6e24d0d1deb007f525680
Ashi Gupta <ashig9981@gmail.com>
2024-11-19 14:17:34 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+
+//write java program for fibonacci series
+
+public class Fibonacci {
+public static void main(String[] args) {
+    int n = 12;
+
+}
+}
+class fibo{
+    int findFibo(int n){
+        int f = a+b;
+        int temp = a;
+        int b  = temp;
+        int a = b;
+
+    }
+}
