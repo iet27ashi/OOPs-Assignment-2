cb894d985326f27594c12c733c7228ed2746a707
Ashi Gupta <ashig9981@gmail.com>
2024-11-19 13:09:25 +0530

    Add files via upload

---
+++ Alt_arr.java
@@ -0,0 +1,9 @@
+public class Alt_arr {
+    public Alt_arr() {
+        System.out.println("This is a 0-arguments constructor.");
+    }
+    public static void main(String[] args) {
+        Alt_arr d = new Alt_arr(); 
+    }
+}
+
